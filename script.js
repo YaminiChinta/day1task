c48c36352e2338676c96d1a5f7dafcfb25595ea6
YaminiChinta <67141828+YaminiChinta@users.noreply.github.com>
2021-04-28 00:11:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+function validateform()
+{
+    var n=document.getElementById("name");
+    var pwd=document.myform.pwd.value;
+    var pno=document.getElementById("phno");
+
+    if(n.value==""){
+        alert("Name can't be blank..");
+        return false;
+    }
+    else if(pwd.length<6){
+        alert("Password must contain atleast 6 characters..");
+        return false;
+    }
+    if(pno.value<10 || pno.value>10)
+    {
+        alert("Invalid Phone Number");
+        return false;
+    }
+    document.getElementById("myform").reset();
+
+}
